81f5565eaaf30c3edfac1842aa3b0118954842d9
fabio.hafreire <fabio.hafreire@sp.senac.br>
2023-03-20 18:53:35 -0300

    Initial commit

---
+++ app/src/main/java/br/senac/exemplointents/explicito/ExExplicitoDetalhe.kt
@@ -0,0 +1,12 @@
+package br.senac.exemplointents.explicito
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.senac.exemplointents.R
+
+class ExExplicitoDetalhe : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ex_explicito_detalhe)
+    }
+}
---
+++ app/src/main/java/br/senac/exemplointents/resposta/ExResposta.kt
@@ -0,0 +1,12 @@
+package br.senac.exemplointents.resposta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.senac.exemplointents.R
+
+class ExResposta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ex_resposta)
+    }
+}
